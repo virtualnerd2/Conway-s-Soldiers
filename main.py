39cd0b105ec18b197c41a4ca040fde64f32a9469
virtualnerd2 <willyram10@gmail.com>
2020-05-16 05:49:48 +0000

    Finished project

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 import pygame
-
-newGame=True
-loggedIn=False
-miniGame=False
+import time
 
 black=(0,0,0)
 white=(255,255,255)
 red=(255,0,0)
+blue = (0,0,255)
+yellow = (255,255,0)
 greyBackground=(203, 206, 203)
 
-# This sets the WIDTH and HEIGHT of each grid location
 width = 65
 height = 65
 radius = 30
-
 margin = 2
 xDistanceFromEdge = 0
-
 gameBoard=[[None]*10 for _ in range(10)] 
-windowSize=[1000, 1000]
+windowSize=[670, 670]
 
 pygame.init()
 screen = pygame.display.set_mode(windowSize)
@@ -26,12 +22,13 @@ pygame.display.set_caption("Draughts Game")
 done = False
 clock = pygame.time.Clock()
 
-# Added helper function.
-def square_colour(row, col):
-    """ Determine colour of game board square from its position. """
-    return white if (row + col) % 2 == 0 else black  # Makes upper-left corner white.
+level = 1
+newGame=True
 
-def boardGui(black,white):
+def square_colour(row, col):
+    return white if (row + col) % 2 == 0 else black  
+
+def boardGui():
     for boardRow in range(10):
         for boardColumn in range(10):
             xCoordinate=((margin+width) * boardColumn + margin)+xDistanceFromEdge
@@ -42,6 +39,8 @@ def boardGui(black,white):
 def piecesGameBoard(gameBoard):
     if newGame:
         newGameBoard(gameBoard)
+    else:
+      drawPieces(gameBoard)
 
 def newGameBoard(gameBoard):
     gameBoard[:] = [[None]*10 for _ in range(10)]  # Empty the game board.
@@ -49,21 +48,66 @@ def newGameBoard(gameBoard):
     for x in range(10):
         for y in range(10):
               if y in range(5,10):
-                  gameBoard[x][y]="Red"
+                gameBoard[x][y]="Red"
+              else:
+                gameBoard[x][y]="Empty"
 
-    drawPieces(gameBoard,black,red)
+    drawPieces(gameBoard)
 
-def drawPieces(gameBoard,black,red):
+def drawPieces(gameBoard):
     for x in range(10):
         for y in range(10):
             xCoordinate=((margin+width) * x + margin+32)+xDistanceFromEdge
             yCoordinate=(margin+height) * y + margin+33
             if gameBoard[x][y]=="Red":
                 pygame.draw.circle(screen,red,(xCoordinate,yCoordinate),radius)
-            if gameBoard[x][y]=="Black":
-                pygame.draw.circle(screen,black,(xCoordinate,yCoordinate),radius)
+            elif gameBoard[x][y]=="Blue":
+                pygame.draw.circle(screen,blue,(xCoordinate,yCoordinate),radius)
 
-# -------- Main Program Loop -----------
+def drawLine(gameBoard):
+  x = 6 - level
+  xCoordinate= (0,   ((margin+width) * x + margin)+xDistanceFromEdge)
+  yCoordinate= (670, ((margin+width) * x + margin)+xDistanceFromEdge)
+  pygame.draw.line(screen, yellow, xCoordinate, yCoordinate, 4)
+
+def checkMove(column, row):
+  if(gameBoard[column][row] != "Empty"):
+    return "NOT VALID"
+  
+  if(row - 2 >= 0 and gameBoard[column][row-2] == "Blue"):
+    return "UP"
+  elif(row + 2 < len(gameBoard[column]) and gameBoard[column][row+2] == "Blue"):
+    return "DOWN"
+  elif(column+2 < len(gameBoard) and gameBoard[column+2][row] == "Blue"):
+    return "LEFT"
+  elif(column-2 >= 0 and gameBoard[column-2][row] == "Blue"):
+    return "RIGHT"
+  else:
+    return "NOT VALID"
+
+def renderImage():
+  screen.fill(greyBackground)
+  boardGui()
+  drawLine(gameBoard)
+  piecesGameBoard(gameBoard)
+  clock.tick(60)
+  pygame.display.flip()
+
+
+selected = False
+def printLevel():
+  if level == 1:
+    print("LEVEL 1: EASY ENOUGH")
+  if level == 2:
+    print("LEVEL 2: KINDA TRICKY")
+  if level == 3:
+    print("LEVEL 3: HEY THIS IS AN ACTUAL CHALLENGE")
+  if level == 4:
+    print("LEVEL 4: THIS IS GETTING OUT OF HAND")
+  if level == 5:
+    print("LEVEL 5: WINNING IS OVERATED")
+
+printLevel()
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -75,13 +119,51 @@ while not done:
             # Change the x/y screen coordinates to grid coordinates
             column = (pos[0]-xDistanceFromEdge) // (width+margin)
             row = pos[1] // (height+margin)
-            print((row, column))
-            gameBoard[column][row] = "Black"
+            print((column, row))
+
+            validMove = checkMove(column, row)
+            if(gameBoard[column][row] == "Blue"):
+              gameBoard[column][row] = "Red"
+              selected = False
+
+            elif (selected and validMove != "NOT VALID"):
+              gameBoard[column][row] = "Red"
+              if(validMove == "UP"):
+                gameBoard[column][row-2] = "Empty"
+                gameBoard[column][row-1] = "Empty"
+              elif(validMove == "DOWN"):
+                gameBoard[column][row+2] = "Empty"
+                gameBoard[column][row+1] = "Empty"
+              elif(validMove == "LEFT"):
+                gameBoard[column+2][row] = "Empty"
+                gameBoard[column+1][row] = "Empty"
+              elif(validMove == "RIGHT"):
+                gameBoard[column-2][row] = "Empty"
+                gameBoard[column-1][row] = "Empty"
+              selected = False
+
+              if(row == 5 - level):
+                print("You did it")
+                print("Level Complete")
+
+                level = level + 1
+                printLevel()
+                
+                renderImage()
+                time.sleep(2)
+                newGame = True
             
-    screen.fill(greyBackground)
-    boardGui(black,white)
-    piecesGameBoard(gameBoard)
-    clock.tick(60)
-    pygame.display.flip()
+
+            elif(selected and validMove == "NOT VALID"):
+                print("Not Valid")
+            elif(not selected and gameBoard[column][row] == "Red" and column < len(gameBoard) and row < len(gameBoard[0])):
+              gameBoard[column][row] = "Blue"
+              selected = True
+        elif pygame.key.get_pressed()[pygame.K_r] == True:
+          newGame = True
+          print("Reset")
+            
+    renderImage()
+    newGame = False
 
 pygame.quit()
